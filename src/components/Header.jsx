65af37b6963652ad6c511e435d485aa82b031edb
fuadslymnv <fuadsuleymanov03@gmail.com>
2023-10-24 08:56:16 +0400

    adding Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+const Header = () => {
+  return <header id="header" >
+    <img src={logo} alt="Logo" />
+    <h1>Investment calculator</h1>
+  </header>;
+};
+
+export default Header;
